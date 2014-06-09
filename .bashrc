29738f8c9c3b9ecb502c3d187cdf86fc5686d0c0
Romain Prignon <pro.rprignon@gmail.com>
2014-06-09 15:40:47 +0200

    fresh start

--- .bashrc
+++ .bashrc
@@ -1,1 +1,3 @@
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color "#272728282222"
+
 [ -n "$PS1" ] && source ~/.bash_profile
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "gnome-terminal-theme-monokai.sh" \
-		--exclude "README.md" -av --no-perms . ~
-	source ~/.bash_profile
+	#rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "gnome-terminal-theme-monokai.sh" \
+	#	--exclude "README.md" -av --no-perms . ~
+	#source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
