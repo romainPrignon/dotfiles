4033c6ee78f5a7a6cd34939baf730282e43773df
Romain Prignon <pro.rprignon@gmail.com>
2016-04-10 18:23:22 +0200

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,11 @@ ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.gitattributes /home
 ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.gitconfig /home/romainprignon/.gitconfig
 ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.gitignore /home/romainprignon/.gitignore
 ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.vimrc /home/romainprignon/.vimrc
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.aliases /home/romainprignon/.aliases
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.bashrc /home/romainprignon/.bashrc
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.exports /home/romainprignon/.exports
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.functions /home/romainprignon/.functions
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.git_prompt /home/romainprignon/.git_prompt
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.bash_prompt /home/romainprignon/.bash_prompt
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.inputrc /home/romainprignon/.inputrc
+ln -sf /home/romainprignon/workspace/romainprignon/dotfiles/.profile /home/romainprignon/.profile
