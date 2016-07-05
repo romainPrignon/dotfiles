ad2c5f9326a89db3ede833180a181df2e4810f0d
Romain Prignon <Romain Prignon>
2016-07-05 21:08:22 +0200

    update bootstrap.bat script

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,4 +1,4 @@
-set dotfile_dir_absolute_path="C:\Users\Romain/workspace/romainprignon\dotfiles"
+set dotfile_dir_absolute_path="C:\Users\Romain\workspace\romainprignon\dotfiles"
 set home_dir_absolute_path="C:\Users\Romain"
 
 cd %home_dir_absolute_path%
@@ -7,6 +7,23 @@ mkdir .composer
 mkdir .ssh
 
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
+
+rmdir %home_dir_absolute_path%\.atom
+del %home_dir_absolute_path%\.composer\composer.json
+rmdir %home_dir_absolute_path%\scripts
+del %home_dir_absolute_path%\.gitattributes
+del %home_dir_absolute_path%\.gitconfig
+del %home_dir_absolute_path%\.gitignore
+del %home_dir_absolute_path%\.vimrc
+del %home_dir_absolute_path%\.aliases
+del %home_dir_absolute_path%\.bashrc
+del %home_dir_absolute_path%\.exports
+del %home_dir_absolute_path%\.exports_windows
+del %home_dir_absolute_path%\.functions
+del %home_dir_absolute_path%\.bash-git-prompt\themes\Single_line_Ubuntu_Romain.bgptheme
+del %home_dir_absolute_path%\.inputrc
+del %home_dir_absolute_path%\.profile
+del %home_dir_absolute_path%\.ssh\config
 
 mklink /D %home_dir_absolute_path%\.atom %dotfile_dir_absolute_path%\.atom
 mklink /H %home_dir_absolute_path%\.composer\composer.json %dotfile_dir_absolute_path%\.composer\composer.json
