4fc036917196e9bf49d44af5d9cdb71be9c8cfcb
Romain Prignon <pro.rprignon@gmail.com>
2016-08-06 12:06:05 +0200

    default to LF in atom and font-size 15

---
+++ .atom/packages/line-ending-selector-unix/lib/line-ending-list-view.js
@@ -0,0 +1,35 @@
+'use babel'
+
+import { SelectListView } from 'atom-space-pen-views'
+
+export default class LineEndingListView extends SelectListView {
+  initialize (callback) {
+    this.callback = callback
+    super.initialize()
+    this.setItems([{name: 'LF', value: '\n'}, {name: 'CRLF', value: '\r\n'}])
+  }
+
+  getFilterKey () {
+    return 'name'
+  }
+
+  viewForItem (ending) {
+    let element = document.createElement('li')
+    element.textContent = ending.name
+    return element
+  }
+
+  cancelled () {
+    this.callback()
+  }
+
+  confirmed (ending) {
+    this.cancel()
+    this.callback(ending.value)
+  }
+
+  reset () {
+    this.filterEditorView.focus()
+    this.populateList()
+  }
+}
