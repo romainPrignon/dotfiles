81c5a762876a35ecf63a2f10c8ff9aae4c29f7ee
Romain Prignon <pro.rprignon@gmail.com>
2017-08-24 17:01:41 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# pre-install
+dpkg-reconfigure keyboard-configuration
+add-apt-repository ppa:ondrej/php
+
+apt-get update
+
+# install: lib
+apt install -y \
+    software-properties-common \
+    build-essential \
+
+# install: runtime
+
+## node
+wget http://git.io/n-install | bash
+n use latest
+
+## php
+apt install -y \
+    php7.1-cli
+    php7.1-curl
+    php7.1-sqlite3
+    php7.1-intl
+    php7.1-mysql
+    php7.1-zip
+    php7.1-mcrypt
+    php7.1-mbstring
+    php7.1-json
+    php7.1-xml
+    php7.1-common
+    php7.1-opcache
+    php7.1-readline
+
+wget https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+## go
+
+## python
+apt install -y \
+    python-pip
+
+## java
+apt install -y \
+    openjdk-8-jdk
+
+# install: app
+apt install -y \
+    curl
+
+# install: desktop
+
+## FIXME: when gnome replace unity
+apt install -y \
+    lightdm \
+    gnome-core \
+    xfonts-base \
+    xserver-xorg
+
+#
+
+
+php7.1-cli
+sublime-text
+gnome-tweak
+ttf-dejavu
+
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
